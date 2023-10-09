90bb505ecba8f925a45611c547ef6ff1e3299288
Dalton Luce <daltonluce42@gmail.com>
2023-10-09 08:37:01 -0400

    Further work on 'units' interface
    * Experimenting with modules

--- src/units.ml
+++ src/units.ml
@@ -1,45 +1,96 @@
-type volume_unit = Teaspoon | Tablespoon | Cup | Pint | Quart | Gallon
-type volume = float * volume_unit
+module type Measurement = sig
+  type units
+  type measure
 
-module UnitMap = Map.Make (struct
-  type t = volume_unit
+  val simplify : measure -> measure
+  val convert : measure -> units -> measure
+  val ( + ) : measure -> measure -> measure
+  val ( - ) : measure -> measure -> measure option
+  val ( > ) : measure -> measure -> bool
+  val ( < ) : measure -> measure -> bool
+  val ( == ) : measure -> measure -> bool
+end
 
-  let compare = compare
-end)
+module Volume = struct
+  type volume_unit =
+    | Teaspoon
+    | Tablespoon
+    | QuaterCup
+    | HalfCup
+    | Cup
+    | Pint
+    | Quart
+    | Gallon
 
-let conversion_map =
-  UnitMap.empty
-  |> UnitMap.add Teaspoon 1.0 (**)
-  |> UnitMap.add Tablespoon 3.0 
-  |> UnitMap.add Cup 16.0 
-  |> UnitMap.add Pint 32.0
-  |> UnitMap.add Quart 64.0 
-  |> UnitMap.add Gallon 256.0
+  type volume = float * volume_unit
 
-let convert (vol, vol_unit) new_unit =
-  let volume_in_teaspons = vol *. UnitMap.find vol_unit conversion_map in
-  let new_volume = volume_in_teaspons /. UnitMap.find new_unit conversion_map in
-  (new_volume, new_unit)
+  (** Map with volume_unit as keys *)
+  module UnitMap = Map.Make (struct
+    type t = volume_unit
 
-let simplify (vol, vol_unit) =
-  let vol_in_teaspons = vol *. UnitMap.find vol_unit conversion_map in
-  let filtered_map =
-    let is_mult x y =
-      match modf (x /. y) with remainer, _ -> remainer == 0.0
+    let compare = compare
+  end)
+
+  (** Map converting a volume_unit to an equivalent number of teaspons *)
+  let conversion_map =
+    UnitMap.empty |> UnitMap.add Teaspoon 1.0 |> UnitMap.add Tablespoon 3.0
+    |> UnitMap.add QuaterCup 4.0 |> UnitMap.add HalfCup 8.0
+    |> UnitMap.add Cup 16.0 |> UnitMap.add Pint 32.0 |> UnitMap.add Quart 64.0
+    |> UnitMap.add Gallon 256.0
+
+  let convert (vol, vol_unit) new_unit =
+    let volume_in_teaspons = vol *. UnitMap.find vol_unit conversion_map in
+    let new_volume =
+      volume_in_teaspons /. UnitMap.find new_unit conversion_map
     in
-    UnitMap.filter (fun _ value -> is_mult value vol_in_teaspons) conversion_map
-  in
-  match UnitMap.max_binding filtered_map with
-  | largest_unit, _ -> convert (vol, vol_unit) largest_unit
+    (new_volume, new_unit)
 
-let ( + ) (vol_a, vol_unit_a) (vol_b, vol_unit_b) =
-  let a = UnitMap.find vol_unit_a conversion_map in
-  let b = UnitMap.find vol_unit_b conversion_map in
-  if a < b then
-    (* vol_unit_a is 'finer' *)
-    let vol_b_converted = vol_b *. (b /. a) in
-    simplify (vol_a +. vol_b_converted, vol_unit_a)
-  else
-    (* vol_unit_b is 'finer *)
-    let vol_a_converted = vol_a *. (a /. b) in
-    simplify (vol_a_converted +. vol_b, vol_unit_b)
+  let simplify (vol, vol_unit) =
+    let vol_in_teaspons = vol *. UnitMap.find vol_unit conversion_map in
+    let filtered_map =
+      let is_mult x y =
+        match modf (x /. y) with remainer, _ -> remainer == 0.0
+      in
+      UnitMap.filter
+        (fun _ value -> is_mult value vol_in_teaspons)
+        conversion_map
+    in
+    match UnitMap.max_binding filtered_map with
+    | largest_unit, _ -> convert (vol, vol_unit) largest_unit
+
+  let ( + ) (vol_a, vol_unit_a) (vol_b, vol_unit_b) =
+    let a = UnitMap.find vol_unit_a conversion_map in
+    let b = UnitMap.find vol_unit_b conversion_map in
+    if a < b then
+      (* vol_unit_a is 'finer' *)
+      let vol_b_converted = vol_b *. (b /. a) in
+      simplify (vol_a +. vol_b_converted, vol_unit_a)
+    else
+      (* vol_unit_b is 'finer *)
+      let vol_a_converted = vol_a *. (a /. b) in
+      simplify (vol_a_converted +. vol_b, vol_unit_b)
+
+  let ( - ) (vol_a, vol_unit_a) (vol_b, vol_unit_b) =
+    let a = UnitMap.find vol_unit_a conversion_map in
+    let b = UnitMap.find vol_unit_b conversion_map in
+    if a < b then
+      (* vol_unit_a is 'finer' *)
+      let vol_b_converted = vol_b *. (b /. a) in
+      simplify (vol_a -. vol_b_converted, vol_unit_a)
+    else
+      (* vol_unit_b is 'finer *)
+      let vol_a_converted = vol_a *. (a /. b) in
+      simplify (vol_a_converted -. vol_b, vol_unit_b)
+
+  let ( > ) (vol_a, vol_unit_a) (vol_b, vol_unit_b) =
+    match (vol_a, vol_unit_a) - (vol_b, vol_unit_b) with
+    | new_vol, _ -> if new_vol > 0.0 then true else false
+
+  let ( > ) (vol_a, vol_unit_a) (vol_b, vol_unit_b) =
+    match (vol_a, vol_unit_a) - (vol_b, vol_unit_b) with
+    | new_vol, _ -> if new_vol < 0.0 then true else false
+
+  let ( == ) (vol_a, vol_unit_a) (vol_b, vol_unit_b) =
+    match (vol_a, vol_unit_a) - (vol_b, vol_unit_b) with
+    | new_vol, _ -> if new_vol == 0.0 then true else false
+end
--- src/units.mli
+++ src/units.mli
@@ -1,22 +1,44 @@
 (** Common cooking units *)
 
-type volume_unit
-(** The unit representation of the volume, e.g. teaspons, cups, etc. *)
+module type Measurement = sig
+  type units
+  (** The unit representation of the measurement, e.g. teaspons, pounds, etc. *)
 
-type volume
-(** An actual quantity of volume *)
+  type measure
+  (** An actual quantity of the measuremnt *)
 
-val reduce : volume -> volume
-(** Reduces a volume to a simpler representation if possible. For example, 
-    [reduce (4.0, Cup)] evaluates to [(1.0, Quart)].*)
+  val simplify : measure -> measure
+  (** Reduces a measurement to a simpler representation if possible. For example, 
+        [reduce (4.0, Cup)] evaluates to [(1.0, Quart)].*)
 
-val convert : volume -> volume_unit -> volume
-(** Convert a volume in one unit to a volume in another unit *)
+  val convert : measure -> units -> measure
+  (** Convert a measurement in one unit to a volume in another unit *)
 
-val ( + ) : volume -> volume -> volume
-(** [vol1 + vol2 ] is the addition of the volumes in the largest possible 
-    unit representation. Returned in simplest form *)
+  val ( + ) : measure -> measure -> measure
+  (** [m1 + m2 ] is the addition of the measurments in the largest possible 
+        unit representation. Returned in simplest form *)
 
-val ( - ) : volume -> volume -> volume option
-(** [vol1 - vol2 ] is the subtraction of the volumes in the largest possible 
-    unit representation. Requires [vol1 >= vol2]. Returned in simplest form *)
+  val ( - ) : measure -> measure -> measure option
+  (** [m1 - m2 ] is the subtraction of the measurements in the largest possible 
+        unit representation. Requires [m1 >= m2]. Returned in simplest form *)
+
+  val ( > ) : measure -> measure -> bool
+  (** [m1 > m2] is [true] if m1 is a greater quantity than m2. *)
+
+  val ( < ) : measure -> measure -> bool
+  (** [m1 < vol2] is [true] if m1 is a lesser quantity than m2. *)
+
+  val ( == ) : measure -> measure -> bool
+  (** [m1 == m2] is [true] if m1 is the same quantity as m2. *)
+end
+
+module Volume : Measurement
+(** Simple implementation of volume measurements *)
+
+module Weight : Measurement
+(** Simple implementation of weight measurements *)
+
+type amount =
+  | Volume of Volume.measure
+  | Weight of Weight.measure
+  | Count of int
