4c90401ec75d740d24cd9ab30fb4b9c18a650d3c
Dalton Luce <daltonluce42@gmail.com>
2023-10-11 17:57:57 -0400

    Added initial tests for Quantity
    * Most failing

--- test/main.ml
+++ test/main.ml
@@ -1,1 +1,29 @@
-open 
+open OUnit2
+open Yum
+open Quantity
+
+let compare_amounts a1 a2 = a1 = a2
+
+let quantity_tests =
+  [
+    ( "Simplify 1" >:: fun _ ->
+      assert_equal ~cmp:compare_amounts
+        (Volume (3.0, Teaspoon))
+        (Volume (1.0, Tablespoon)) );
+    ( "Simplify 2" >:: fun _ ->
+      assert_equal ~cmp:compare_amounts
+        (Volume (16.0, Cup))
+        (Volume (1.0, Gallon)) );
+    ( "Simplify 3" >:: fun _ ->
+      assert_equal ~cmp:compare_amounts (Mass (16.0, Ounce)) (Mass (1.0, Pound))
+    );
+    ( "Equality operator 1" >:: fun _ ->
+      assert_equal true (Volume (1.0, Teaspoon) = Volume (1.0, Teaspoon)) );
+    ( "Equality operator 2" >:: fun _ ->
+      assert_equal true (Volume (5.5, Gallon) = Volume (5.5, Gallon)) );
+    ( "Equality operator 3" >:: fun _ ->
+      assert_equal true (Volume (3.0, Teaspoon) = Volume (1.0, Tablespoon)) );
+  ]
+
+let suite = "test suite for A2" >::: List.flatten [ quantity_tests ]
+let () = run_test_tt_main suite
