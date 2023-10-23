b088329f0407cebff46a7b2c0adde92952213117
Dalton Luce <daltonluce42@gmail.com>
2023-10-23 18:16:17 -0400

    Make it so that pantry combines units

--- lib/pantry.ml
+++ lib/pantry.ml
@@ -8,18 +8,21 @@ let empty = []
 let add (pantry : t) (ing : Ingredient.ingredient) (a : Quantity.amount) : t =
   match List.assoc_opt ing pantry with
   | Some old_a -> (
+      let pantry_without_assoc = List.remove_assoc ing pantry in
       match old_a with
       | Volume v1 -> (
           match a with
-          | Volume v2 -> (ing, Volume (Quantity.Volume.add v1 v2)) :: pantry
+          | Volume v2 ->
+              (ing, Volume (Quantity.Volume.add v1 v2)) :: pantry_without_assoc
           | _ -> pantry)
       | Mass m1 -> (
           match a with
-          | Mass m2 -> (ing, Mass (Quantity.Mass.add m1 m2)) :: pantry
+          | Mass m2 ->
+              (ing, Mass (Quantity.Mass.add m1 m2)) :: pantry_without_assoc
           | _ -> pantry)
       | Count c1 -> (
           match a with
-          | Count c2 -> (ing, Count (c1 +. c2)) :: pantry
+          | Count c2 -> (ing, Count (c1 +. c2)) :: pantry_without_assoc
           | _ -> pantry))
   | None -> (ing, a) :: pantry
 
