2e9566d8eca1fca7892442d349c46846826dde2a
Andrew <al2245@cornell.edu>
2023-11-14 22:23:12 -0500

    Implemented functionality for adding and removing ingredients

--- bin/gui.ml
+++ bin/gui.ml
@@ -8,62 +8,20 @@ module W = Widget
 
 (** [getValidIngredient ()] prompts the user for an ingredient and returns
     that ingredient if it is valid. Otherwise, it prompts the user again. *)
-let rec getValidIngredient () =
-  print_endline "\nWhat food would you like to add: ";
+(** TODO: Fix this implementation for an invalid input. Defaults to Apple. *)
+let rec getValidIngredient (input : string) : ingredient =
   match read_line () |> Ingredient.of_string with
-  | None ->
-    print_endline "Invalid ingredient. Please try again.\n";
-    getValidIngredient ()
+  | None -> Apple
   | Some x -> x
 
 (** [getValidQuantity ()] prompts the user for a quantity and returns
     that quantity if it is valid. Otherwise, it prompts the user again. *)
-let rec getValidQuantity () =
-  print_endline "\nHow much of the food do you want to add?";
-  match read_line () |> Quantity.of_string with
-  | None ->
-    print_endline "Invalid quantity. Please try again.\n";
-    getValidQuantity ()
+(** TODO: Infinite recursion if invalid quantity. *)
+let rec getValidQuantity (input : string) =
+  match input |> Quantity.of_string with
+  | None -> getValidQuantity input
   | Some x -> x
 
-(** [action pantry] is the main loop of the program. It prompts the user for
-    an action and then executes that action, then recursively loops. *)
-let rec action pantry =
-  print_endline "Please enter what you would like to do: ";
-  print_endline "\"add\", \"remove\", \"display\", \"reset\", or \"quit\"";
-  print_string "> ";
-
-  match read_line () with
-  | "add" ->
-    let ingredient = getValidIngredient () in
-    let quantity = getValidQuantity () in
-    let new_pantry = Pantry.add pantry ingredient quantity in
-    print_endline "Ingredient added.\n";
-    action new_pantry
-
-  | "remove" ->
-    let ingredient = getValidIngredient () in
-    let quantity = getValidQuantity () in
-    let new_pantry = Pantry.remove pantry ingredient quantity in
-    print_endline "Ingredient removed.\n";
-    action new_pantry
-
-  | "display" ->
-    let display_text = Pantry.display pantry in
-    if display_text = "" then print_endline "Your pantry is empty.\n"
-    else print_endline (display_text ^ "\n");
-    action pantry
-
-  | "reset" ->
-    print_endline "Pantry reset.\n";
-    action (Pantry.reset pantry)
-
-  | "quit" ->
-    print_endline "Goodbye!\n";
-    ()
-  | _ ->
-    print_endline "Invalid action.\n";
-    action pantry
 
 let text_input () = 
   let input = W.text_input ~max_size:200 ~prompt:"Enter your name" () in
@@ -75,12 +33,43 @@ let text_input () =
     let board = Bogue.of_layout layout in
     Bogue.run ~before_display board
 
+let rec action_choice pantry = 
+  let add_function pantry ingredient quantity = 
+    let ingredient = getValidIngredient ingredient in 
+    let quantity = getValidQuantity quantity in
+      let new_pantry = Pantry.add pantry ingredient quantity in
+      action_choice new_pantry in
+  
+  let remove_function pantry ingredient quantity = 
+    let ingredient = getValidIngredient ingredient in 
+    let quantity = getValidQuantity quantity in
+      let new_pantry = Pantry.remove pantry ingredient quantity in
+      action_choice new_pantry in
+
+(** A shared page for adding and removing an ingredient from the pantry.*)
+  let add_remove_input (action : string) pantry = 
+    let food_input = W.text_input ~prompt: "Enter a valid food" () in 
+    let amount_input = W.text_input ~prompt: "Enter a valid quantity" () in 
+    let food_label = W.label ("What food would you like to " ^ action ^ "?") in 
+    let amount_label = W.label ("How much would you like to " ^ action ^ "?") in
+    let submit = W.button   "Submit" in
+      if action = "add" then 
+      W.on_click submit ~click: (fun _ -> add_function pantry (W.get_text food_input) (W.get_text amount_input))
+      else 
+      W.on_click submit ~click: (fun _ -> remove_function pantry (W.get_text food_input) (W.get_text amount_input));
+    let row1 = L.flat_of_w [food_label; food_input] in
+    let row2 = L.flat_of_w [amount_label; amount_input] in 
+    let layout = L.tower [row1; row2; L.resident submit] in
+    let board = Bogue.of_layout layout in Bogue.run board in
+
+  
 (** The main page of the program. It provides various buttons that performs various actions.
     The default page that every action returns to after completely executing. *)
-let action_choice () = 
   let label = W.label "What would you like to do?" in
   let add_button = W.button "Add" in
+    W.on_click add_button ~click: (fun _ -> add_remove_input "add" pantry);
   let remove_button = W.button "Remove" in
+    W.on_click remove_button ~click: (fun _ -> add_remove_input "remove" pantry);
   let display_button = W.button "Display" in
   let reset_button = W.button "Reset" in
   let quit_button = W.button "Quit" in 
@@ -94,4 +83,5 @@ let () =
   print_endline "Welcome to Yummy!";
   let pantry = Pantry.empty in
   action pantry *)
-    action_choice ()
+    let pantry = Pantry.empty in
+    action_choice pantry
