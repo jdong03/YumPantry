373ba49b023ba2e66d522292d2558bedbaad474c
suyean <sl2658@cornell.edu>
2023-12-06 00:41:54 -0500

    Fixed bugs in add, remove, and possible recipes in cmd line

--- bin/main.ml
+++ bin/main.ml
@@ -5,14 +5,15 @@ open Recipe
 open Quantity
 open Match
 
-(** [getValidIngredient ()] prompts the user for an ingredient and returns
-    that ingredient if it is valid. Otherwise, it prompts the user again. *)
-let rec getValidIngredient () =
-  print_endline "\nWhat food would you like to add: ";
+(** [getValidIngredient action] prompts the user for an ingredient and returns
+    that ingredient if it is valid. Otherwise, it prompts the user again.
+    [action] is a string that is either "add" or "remove". *)
+let rec getValidIngredient (action : string) =
+  print_endline ("\nWhat food would you like to " ^action ^ ": ");
   match read_line () |> Ingredient.of_string with
   | None ->
       print_endline "Invalid ingredient. Please try again.\n";
-      getValidIngredient ()
+      getValidIngredient action
   | Some ingredient -> ingredient
 
 (** Print all elements in a list using the given to_string method [f]*)
@@ -34,19 +35,20 @@ let rec print_all_string lst =
 let print_all_ingredients () = print_all all_ingredients Ingredient.to_string
 let print_all_recipes () = print_all all_recipes Recipe.to_string
 
-(** [getValidQuantity ()] prompts the user for a quantity and returns
-    that quantity if it is valid. Otherwise, it prompts the user again. *)
-let rec getValidQuantity (ingredient : Ingredient.t) () =
+(** [getValidQuantity action] prompts the user for a quantity and returns
+    that quantity if it is valid. Otherwise, it prompts the user again.
+    [action] is a string that is either "add" or "remove". *)
+let rec getValidQuantity (ingredient : Ingredient.t) (action : string) =
   print_endline
     ("\nHow much "
     ^ Ingredient.to_string ingredient
-    ^ " do you want to add? (Measured in "
+    ^ " do you want to " ^ action ^ "? (Measured in "
     ^ Quantity.measurement_type (Ingredient.default_units ingredient)
     ^ ")");
   match read_line () |> Quantity.of_string with
   | None ->
       print_endline "Invalid quantity. Please try again.\n";
-      getValidQuantity ingredient ()
+      getValidQuantity ingredient action
   | Some x -> x
 
 (** [getValidRecipe ()] prompts the user for a recipe and returns
@@ -70,15 +72,15 @@ let rec action pantry =
   print_string "> ";
 
   match read_line () with
-  | "add" ->
-      let ingredient = getValidIngredient () in
-      let quantity = getValidQuantity ingredient () in
+  | "add" as add ->
+      let ingredient = getValidIngredient add in
+      let quantity = getValidQuantity ingredient add in
       let new_pantry = Pantry.add pantry ingredient quantity in
       print_endline "Ingredient added.";
       action new_pantry
-  | "remove" ->
-      let ingredient = getValidIngredient () in
-      let quantity = getValidQuantity ingredient () in
+  | "remove" as remove ->
+      let ingredient = getValidIngredient remove in
+      let quantity = getValidQuantity ingredient remove in
       let new_pantry = Pantry.remove pantry ingredient quantity in
       print_endline "Ingredient removed.";
       action new_pantry
@@ -100,21 +102,26 @@ let rec action pantry =
       action (Pantry.reset pantry)
   | "possible recipes" ->
         let possible_recipes = Match.display_all_matches pantry all_recipes in
-        if possible_recipes = [] then
-            print_endline "You don't have enough ingredients to make any recipes."
-        else 
-            print_endline "Possible recipes:\n";
-            print_all_string possible_recipes;
-            let recipe = getValidRecipe () in
-            print_endline "\nHere is the recipe:\n";
-            print_endline (Recipe.to_string recipe);
-        action pantry
+        if possible_recipes = [] then noPossibleRecipes pantry
+        else chooseRecipe possible_recipes pantry
   | "quit" ->
       print_endline "Goodbye!\n";
       ()
   | _ ->
       print_endline "Invalid action.";
       action pantry
+
+and noPossibleRecipes pantry =
+  print_endline "You don't have enough ingredients to make any recipes.";
+  action pantry
+
+and chooseRecipe possible_recipes pantry =
+    print_endline "Possible recipes:\n";
+    print_all_string possible_recipes;
+    let recipe = getValidRecipe () in
+    print_endline "\nHere is the recipe:\n";
+    print_endline (Recipe.to_string recipe);
+    action pantry
 
 (*********** command line interface ***********)
 let () =
