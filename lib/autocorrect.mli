0713abf49cbdd275244ae79a3744fb7f94d7aa48
Andrew <al2245@cornell.edu>
2023-12-06 17:16:56 -0500

    Added autocorrect functionality to Ingredient module so it works with the GUI. Deleted Autocorrect module.

--- lib/autocorrect.mli
+++
@@ -1,9 +0,0 @@
-open Ingredient
-
-val calc_score : string -> string -> int
-(* [calc_score s1 s2] calculates the Levenshtein distance of s1 and s2. *)
-
-val match_string : string -> Ingredient.t option
-(* [match_string s] "autocorrects" s by matching it to an ingredient in 
-   Ingredient.al_ingredients. Returns the ingredient that has the lowest 
-   Levenshtein distance with s if the distance is less than 3. Returns None otherwise. *)
