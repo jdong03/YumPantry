a192fed0216e5616d40bb346f29f4ddfccb1f09b
Jason <jd876@cornell.edu>
2023-12-07 19:25:20 -0500

    Some more documentation changes/updates

--- lib/match.mli
+++ lib/match.mli
@@ -4,15 +4,15 @@ open Recipe
 (** Module for matching recipes with available ingredients in a pantry. *)
 
 val can_make_recipe : Recipe.t -> Pantry.t -> bool
-(** [can_make r p] is true if the ingredients in [p] can make the recipe [r] *)
+(** [can_make r p] is true if the ingredients in [p] can make the recipe [r]. *)
 
 val get_all_matches : Pantry.t -> Recipe.t list -> Recipe.t list
 (** [get_all_matches p rl] is a list of all the recipes in [rl] that can be made
-    with the ingredients in [p] *)
+    with the ingredients in [p]. *)
 
 val display_all_matches : Pantry.t -> Recipe.t list -> string list
 (** [display_all_matches p rl] is a string list of all the names of the recipes
-    in [rl] that can be made with the ingredients in [p] *)
+    in [rl] that can be made with the ingredients in [p]. *)
 
 val get_selected_recipe : string -> Recipe.t list -> Recipe.t option
-(** [get_selected_recipe s rl] is the recipe in [rl] whose name is [s] *)
+(** [get_selected_recipe s rl] is the recipe in [rl] whose name is [s]. *)
--- lib/recipe.mli
+++ lib/recipe.mli
@@ -5,33 +5,32 @@
     to interact with and retrieve information from this type.
 *)
 
-(* Type definition for a recipe *)
 type t
-(** The type representing a recipe *)
+(** The type representing a recipe. *)
 
 val to_string : t -> string
-(** Convert a recipe to a string representation *)
+(** Convert a recipe to a string representation. *)
 
 val title : t -> string
-(** The title of the recipe *)
+(** The title of the recipe. *)
 
 val servings : t -> int
-(** The number of servings the recipe makes *)
+(** The number of servings the recipe makes. *)
 
 val prep_time : t -> float
-(** The time it takes to prepare the recipe *)
+(** The time it takes to prepare the recipe. *)
 
 val cook_time : t -> float
-(** The cook time of the recipe *)
+(** The time it takes to cook the recipe. *)
 
 val instructions : t -> string
-(** The instructions for the recipe *)
+(** The instructions for the recipe. *)
 
 val ingredients : t -> (Ingredient.t * Quantity.t) list
-(** The list of ingredients in their respective quantities for this recipe *)
+(** The list of ingredients in their respective quantities for this recipe. *)
 
 val all_recipes : t list
-(** A list of all recipes in data/recipes.recipes.json*)
+(** A list of all recipes in data/recipes.recipes.json. *)
 
 val get_information_from_name : string -> string
-(** Get the instructions for a recipe from its name *)
+(** Get the instructions for a recipe from its name. *)
